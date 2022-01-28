a21e31b1a449b64ac85aacc9ff412feaffc2779c
Engineer-Hohenzollern <95528781+Engineer-Hohenzollern@users.noreply.github.com>
2022-01-28 15:16:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ def number_borrowers(loanid):
     loanid = ['2d6acb8c-0396-4696-abab-b50c548314de', 'f522d624-db0f-47be-ab02-7e7c0220ef61',
               'f522d624-db0f-47be-ab02-7e7c0220ef61', "f522d624-db0f-47be-ab02-7e7c0220ef61",
               "f522d624-db0f-47be-ab02-7e7c0220ef61", '0d0fa588-ca95-4920-8aac-0c446ea4a7cd',
-              'cafffcae - 8531 - 41a8 - bbd0 - cc03e3a66818']
-    loanid =loanid[1]
+              'cafffcae - 8531 - 41a8 - bbd0 - cc03e3a66818','b4c8eba2-63b9-42db-a45f-b3358541d369']
+    loanid = loanid[1]
     url = "https://api.elliemae.com/encompass/v1/loans/" + loanid + "/applications"
     auth = authenticate.Authenticate()
 
@@ -17,28 +17,25 @@ def number_borrowers(loanid):
     # print(a)
     # for r in a:
     #     print(r)
-    count=0
+    count = 0
     for r in a:
 
-        d=a[r]
+        d = a[r]
         if type(d) is dict:
             print(r)
             if "isBorrower" in d:
-                if d["isBorrower"]== True:
-                    count+=1
+                if d["isBorrower"] == True:
+                    count += 1
+        if type(d) is list:
+            print(d)
+            for r in d:
+                print(r)
+                if type(r) is dict:
+                    if "isBorrower" in r:
+                        if r["isBorrower"] == True:
+                            count += 1
+
+
     return count
-
-    # c = a["aUSTrackingLogs"]
-    # v=len(c)
-    # print(v)
-    # print(c)
-    # num = 1
-    # print(" the borrower name is ", c["logBorrowerName"])
-    # if "logCoborrowerName" in c.keys():
-    #     print(" the CO-borrower name is ", c["logCoborrowerName"])
-    #     num = 2
-    #
-    # return num
-
 
 print(number_borrowers(loanid=2))
